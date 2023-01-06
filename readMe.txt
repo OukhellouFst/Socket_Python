dab3baebf349c8d6fc94fa2f169f32cd564aad6d
oukhellouFst <oukhellou.fst@gmail.com>
2023-01-06 02:07:19 +0100

    Adding A simple server , and editing the client socket for requesting from this server

--- readMe.txt
+++ readMe.txt
@@ -4,3 +4,6 @@ so we will start by establishing a connection to a remote server , and then we g
 and after that we will save the received response in a data variable so we can use it later .
 
 Simple as that :) 
+
+
+Now we will create a simple server to fetch the request and response back to that request .
--- socket1.py
+++ socket1.py
@@ -1,8 +1,8 @@
 import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode()
+mysock.connect(('127.0.0.1', 9000))
+cmd = 'GET http://127.0.0.1/index.html HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 
 while True:
